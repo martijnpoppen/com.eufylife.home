66292252843e563d5720016c24956d5825e4eba5
Martijn Poppen <m.poppen@ziggo.nl>
2021-10-06 23:35:00 +0200

    NEW: add clean speed for Robovac X8

--- drivers/main-device.js
+++ drivers/main-device.js
@@ -87,6 +87,7 @@ module.exports = class mainDevice extends Homey.Device {
             const batteryLevel = await this.eufyRoboVac.getBatteyLevel() || 1;
             const workStatus = await this.eufyRoboVac.getWorkStatus();
             const workMode = await this.eufyRoboVac.getWorkMode();
+            const cleanSpeed = await this.eufyRoboVac.getCleanSpeed();
             const errors = await this.eufyRoboVac.getErrorCode();
             const cleanState = await this.eufyRoboVac.getPlayPause();
             const currentState = GET_STATE[workStatus] || GET_STATE[workMode];
@@ -94,6 +95,7 @@ module.exports = class mainDevice extends Homey.Device {
             this.homey.app.log(`[Device] ${this.getName()} - setCapabilityValues - workStatus`, workStatus);
             this.homey.app.log(`[Device] ${this.getName()} - setCapabilityValues - workMode`, workMode);
             this.homey.app.log(`[Device] ${this.getName()} - setCapabilityValues - currentState`, currentState);
+            this.homey.app.log(`[Device] ${this.getName()} - setCapabilityValues - cleanSpeed`, cleanSpeed);
             this.homey.app.log(`[Device] ${this.getName()} - setCapabilityValues - errors`, errors);
 
             await this.setCapabilityValue('measure_battery', parseInt(batteryLevel));
@@ -113,6 +115,10 @@ module.exports = class mainDevice extends Homey.Device {
             if(workMode) {
                 await this.setCapabilityValue('measure_work_mode', workMode);
             }
+
+            if(cleanSpeed) {
+                await this.setCapabilityValue('measure_clean_speed', `${cleanSpeed}`);
+            }
             
             await this.setCapabilityValue('measure_error', !errors ? 'no_error' : errors);
         
@@ -127,7 +133,7 @@ module.exports = class mainDevice extends Homey.Device {
 
     async setCapabilityValuesInterval() {
         try {  
-            const REFRESH_INTERVAL = 1000 * (3 * 60);
+            const REFRESH_INTERVAL = 1000 * (1 * 60);
 
             this.homey.app.log(`[Device] ${this.getName()} - onPollInterval =>`, REFRESH_INTERVAL);
             this.onPollInterval = setInterval(this.setCapabilityValues.bind(this), REFRESH_INTERVAL);
